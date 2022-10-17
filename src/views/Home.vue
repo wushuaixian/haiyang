bc1b6ca5970df230c1b0bfdeeb455f95e7614f63
wushuaixian <1205904557@qq.com>
2022-10-17 10:41:42 +0800

    添加禁止用户名称选中文字

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -483,6 +483,11 @@ export default {
 .myButton {
   width: 100px;
   text-align: center;
+  -moz-user-select: none; /*火狐*/
+  -webkit-user-select: none; /*webkit浏览器*/
+  -ms-user-select: none; /*IE10*/
+  -khtml-user-select: none; /*早期浏览器*/
+  user-select: none;
 }
 .myButton:hover {
   // background-color: #5cbf2a;
